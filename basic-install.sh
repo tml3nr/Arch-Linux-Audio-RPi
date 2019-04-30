38eb368be11faa251bed925cf427c4424b9f9708
Edward <40375833+dynobot@users.noreply.github.com>
2019-04-29 20:21:09 -0700

    Update basic-install.sh
    revert back to reboot when done

--- basic-install.sh
+++ basic-install.sh
@@ -1,46 +1,42 @@
-#!/bin/bash
+ 1 #!/bin/bash
+ 2
+ 3 border()
+ 4 {
+ 5     local title="| $1 |"
+ 6     local edge=${title//?/-}
+ 7     echo -e "${edge}\n${title}\n${edge}"
+ 8     sleep 1
+ 9 }
+10
+11 border 'Downloading Sound File'
+12
+13 wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/Sound.$
+14 chmod 755 /usr/bin/Sound.sh
+15
+16 border 'Increasing Sound Group Priority'
+17
+18 [[ -f /etc/security/limits.conf ]] && mv /etc/security/limits.conf /etc/s$
+19 echo '#New Limits' > /etc/security/limits.conf
+20 echo '@audio - rtprio 99' >> /etc/security/limits.conf
+21 echo '@audio - memlock 512000' >> /etc/security/limits.conf
+22 echo '@audio - nice -20' >> /etc/security/limits.conf
+23
+24 border 'Improving Network Latency'
+25
+26 echo "#New Network Latency" > /etc/sysctl.d/network-latency.conf
+27 echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
+28 echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
+29
+30 border 'Creating System Service'
+31
+32 [[ -f /etc/rc.local ]] || echo -e '#/bin/bash\n\nexit 0' > /etc/rc.local
+33 grep -q '/usr/bin/Sound.sh' /etc/rc.local || sed -i '\|^#!/bin/.*sh|a\/us$
+34 chmod +x /etc/rc.local
+35 #systemctl enable rc-local || systemctl enable rc.local
+36
+37 border 'Rebooting System'
+38
+39
+40 reboot
+41
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
-  exit
-fi
-
-border()
-{
-    local title="| $1 |"
-    local edge=${title//?/-}
-    echo -e "${edge}\n${title}\n${edge}"
-    sleep 1
-}
-
-border 'Downloading Sound File'
-
-wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/Sound.sh -O /usr/bin/Sound.sh
-chmod 755 /usr/bin/Sound.sh
-
-border 'Increasing Sound Group Priority'
-
-[[ -f /etc/security/limits.conf ]] && mv /etc/security/limits.conf /etc/security/limits.conf.bak
-echo '#New Limits' > /etc/security/limits.conf
-echo '@audio - rtprio 99' >> /etc/security/limits.conf
-echo '@audio - memlock 512000' >> /etc/security/limits.conf
-echo '@audio - nice -20' >> /etc/security/limits.conf
-
-border 'Improving Network Latency'
-
-echo "#New Network Latency" > /etc/sysctl.d/network-latency.conf
-echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
-echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
-
-border 'Creating System Service'
-
-[[ -f /etc/rc.local ]] || echo -e '#/bin/bash\n\nexit 0' > /etc/rc.local
-grep -q '/usr/bin/Sound.sh' /etc/rc.local || sed -i '\|^#!/bin/.*sh|a\/usr/bin/Sound.sh' /etc/rc.local
-chmod +x /etc/rc.local
-#systemctl enable rc-local || systemctl enable rc.local
-
-border 'Activating new settings'
-sysctl net.core.rmem_max=16777216 net.core.wmem_max=16777216
-/usr/bin/Sound.sh
-
-border 'Please reboot the system'
