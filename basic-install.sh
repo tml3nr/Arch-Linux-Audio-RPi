ef8646e2a5756b574b470fad5a17a54f3fa031ec
Edward <40375833+dynobot@users.noreply.github.com>
2019-04-29 20:35:59 -0700

    Update basic-install.sh

--- basic-install.sh
+++ basic-install.sh
@@ -6,35 +6,34 @@ border()
     local edge=${title//?/-}
     echo -e "${edge}\n${title}\n${edge}"
     sleep 1
-    }
+}
 
- border 'Downloading Sound File'
+border 'Downloading Sound File'
  
- wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/Sound.$
- chmod 755 /usr/bin/Sound.sh
+wget https://github.com/dynobot/Linux-Audio-Adjustments/raw/master/Sound.$
+chmod 755 /usr/bin/Sound.sh
 
- border 'Increasing Sound Group Priority'
+border 'Increasing Sound Group Priority'
 
- [[ -f /etc/security/limits.conf ]] && mv /etc/security/limits.conf /etc/s$
- echo '#New Limits' > /etc/security/limits.conf
- echo '@audio - rtprio 99' >> /etc/security/limits.conf
- echo '@audio - memlock 512000' >> /etc/security/limits.conf
- echo '@audio - nice -20' >> /etc/security/limits.conf
+[[ -f /etc/security/limits.conf ]] && mv /etc/security/limits.conf /etc/s$
+echo '#New Limits' > /etc/security/limits.conf
+echo '@audio - rtprio 99' >> /etc/security/limits.conf
+echo '@audio - memlock 512000' >> /etc/security/limits.conf
+echo '@audio - nice -20' >> /etc/security/limits.conf
 
- border 'Improving Network Latency'
+border 'Improving Network Latency'
 
- echo "#New Network Latency" > /etc/sysctl.d/network-latency.conf
- echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
- echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
+echo "#New Network Latency" > /etc/sysctl.d/network-latency.conf
+echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
+echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
 
- border 'Creating System Service'
+border 'Creating System Service'
 
- [[ -f /etc/rc.local ]] || echo -e '#/bin/bash\n\nexit 0' > /etc/rc.local
- grep -q '/usr/bin/Sound.sh' /etc/rc.local || sed -i '\|^#!/bin/.*sh|a\/us$
- chmod +x /etc/rc.local
- #systemctl enable rc-local || systemctl enable rc.local
+[[ -f /etc/rc.local ]] || echo -e '#/bin/bash\n\nexit 0' > /etc/rc.local
+grep -q '/usr/bin/Sound.sh' /etc/rc.local || sed -i '\|^#!/bin/.*sh|a\/us$
+chmod +x /etc/rc.local
+#systemctl enable rc-local || systemctl enable rc.local
 
- border 'Rebooting System'
+border 'Rebooting System'
 
-
- reboot
+reboot
