d15db24832fe2a882838be47b8b05dd15addba1d
Edward <40375833+dynobot@users.noreply.github.com>
2019-09-04 06:26:07 -0700

    Update Sound.sh

--- Sound.sh
+++ Sound.sh
@@ -15,7 +15,6 @@ chrt -f -p 54 $(pgrep ksoftirqd/3)
 #echo 0x0 > /sys/devices/platform/soc/3f980000.usb/buspower
 
 #Reduce operating system latency
-#echo noop > /sys/block/mmcblk0/queue/scheduler
 echo 1000000 > /proc/sys/kernel/sched_latency_ns
 echo 100000 > /proc/sys/kernel/sched_min_granularity_ns
 echo 25000 > /proc/sys/kernel/sched_wakeup_granularity_ns
