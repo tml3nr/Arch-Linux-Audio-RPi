917952a03b1edf6428dc83e5de102da08ff0b1dd
Edward <40375833+dynobot@users.noreply.github.com>
2019-09-07 11:57:34 -0700

    Update Sound.sh

--- Sound.sh
+++ Sound.sh
@@ -2,10 +2,10 @@
 #!/bin/bash
 
 #Reduce Audio thread latency
-chrt -f -p 54 $(pgrep ksoftirqd/0)
-chrt -f -p 54 $(pgrep ksoftirqd/1)
-chrt -f -p 54 $(pgrep ksoftirqd/2)
-chrt -f -p 54 $(pgrep ksoftirqd/3)
+chrt -f -p 74 $(pgrep ksoftirqd/0)
+chrt -f -p 74 $(pgrep ksoftirqd/1)
+chrt -f -p 74 $(pgrep ksoftirqd/2)
+chrt -f -p 74 $(pgrep ksoftirqd/3)
 
 #Uncomment for MPD Affinity and Priority
 #chrt -f -p 81 $(pidof mpd)
@@ -15,9 +15,9 @@ chrt -f -p 54 $(pgrep ksoftirqd/3)
 #echo 0x0 > /sys/devices/platform/soc/3f980000.usb/buspower
 
 #Reduce operating system latency
-#echo 1 > /sys/block/mmcblk0/queue/iosched/fifo_batch
-#echo 10 > /sys/block/mmcblk0/queue/iosched/read_expire
-#echo 50 > /sys/block/mmcblk0/queue/iosched/write_expire
+echo 10 > /sys/block/mmcblk0/queue/iosched/fifo_batch
+echo 10 > /sys/block/mmcblk0/queue/iosched/read_expire
+echo 50 > /sys/block/mmcblk0/queue/iosched/write_expire
 echo 1000000 > /proc/sys/kernel/sched_latency_ns
 echo 100000 > /proc/sys/kernel/sched_min_granularity_ns
 echo 25000 > /proc/sys/kernel/sched_wakeup_granularity_ns
