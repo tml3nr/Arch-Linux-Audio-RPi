2106d724c32995b9e03c0980bde35b23ca050526
Edward <40375833+dynobot@users.noreply.github.com>
2019-09-08 16:05:30 -0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -16,7 +16,9 @@ border 'Removing Linux Audio Tuning'
 [[ -f /etc/systemd/system/sound.service ]] && rm /etc/systemd/system/sound.service
 [[ -f /boot/config.txt ]] && rm /boot/config.txt
 [[ -f /boot/config.txt.bak ]] && mv /boot/config.txt.bak /boot/config.txt
-
+[[ -f /etc/systemd/system/spotifyd.service ]] && rm /etc/systemd/system/spotifyd.service
+[[ -f /usr/bin/spotifyd ]] && rm /usr/bin/spotifyd
+[[ -f ~/.config/spotifyd/spotifyd.conf ]] && rm ~/.config/spotifyd/spotifyd.conf
 
 border 'Cleaning up & Rebooting System'
 sleep 1
