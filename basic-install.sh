e4f3614f85e8410349633fdebb158d6ff56a01f9
Edward <40375833+dynobot@users.noreply.github.com>
2019-10-02 16:19:54 -0700

    Update basic-install.sh

--- basic-install.sh
+++ basic-install.sh
@@ -33,7 +33,7 @@ sleep 1
 wget https://github.com/dynobot/Arch-Linux-Audio-RPi/raw/master/sound.service -O /etc/systemd/system/sound.service
 systemctl enable sound.service
 
-border 'Cleaning Up & Rebooting System Enjoy the Music'
+border 'Cleaning Up'
 sleep 3
 rm basic-install.sh
 border 'Rebooting System Enjoy the Music'
