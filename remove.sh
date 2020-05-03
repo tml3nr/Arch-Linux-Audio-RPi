50bb9d4aab49e00109f23e7ff8d7639a7b12341c
Edward <40375833+dynobot@users.noreply.github.com>
2020-05-03 10:23:24 -0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -10,17 +10,15 @@ border()
 
 border 'Removing Linux Audio Tuning'
 
-[[ -f /usr/bin/Sound.sh ]] && rm /usr/bin/Sound.sh
-[[ -f /etc/sysctl.d/network-latency.conf ]] && rm /etc/sysctl.d/network-latency.conf
+[[ -f /usr/bin/archphile-optimize ]] && rm /usr/bin/archphile-optimize
+[[ -f /etc/sysctl.d/archphile-network.conf ]] && rm /etc/sysctl.d/archphile-network.conf
+[[ -f /etc/security/limits.conf ]] && rm /etc/security/limits.conf
 [[ -f /etc/security/limits.conf.bak ]] && mv /etc/security/limits.conf.bak /etc/security/limits.conf
-[[ -f /etc/systemd/system/sound.service ]] && rm /etc/systemd/system/sound.service
-[[ -f /boot/config.txt ]] && rm /boot/config.txt
-[[ -f /boot/config.txt.bak ]] && mv /boot/config.txt.bak /boot/config.txt
-[[ -f /etc/systemd/system/spotifyd.service ]] && rm /etc/systemd/system/spotifyd.service
-[[ -f /usr/bin/spotifyd ]] && rm /usr/bin/spotifyd
-[[ -f ~/.config/spotifyd/spotifyd.conf ]] && rm ~/.config/spotifyd/spotifyd.conf
+[[ -f /usr/bin/archphile-optimize.bak ]] && mv /usr/bin/archphile-optimize.bak /usr/bin/archphile-optimize
+[[ -f /etc/sysctl.d/archphile-network.conf.bak ]] && mv /etc/sysctl.d/archphile-network.conf.bak /etc/sysctl.d/archphile-network.conf
 
 border 'Cleaning up & Rebooting System'
 sleep 1
 rm remove.sh
 reboot
+
