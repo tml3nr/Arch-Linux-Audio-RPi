0ca808b2446bb3de53e99cf10bd8b16c061a1745
Edward <40375833+dynobot@users.noreply.github.com>
2020-05-03 07:51:08 -0700

    Update basic-install.sh

--- basic-install.sh
+++ basic-install.sh
@@ -10,7 +10,7 @@ border()
 
 border 'Downloading Sound File'
 sleep 1
-mv /usr/bin/archphile-optimize /usr/bin/archphile-optimize.bak
+[[ -f /usr/bin/archphile-optimize ]] && mv /usr/bin/archphile-optimize /usr/bin/archphile-optimize.bak
 wget https://github.com/dynobot/Arch-Linux-Audio-RPi/raw/beta/archphile-optimize -O /usr/bin/archphile-optimize
 chmod 755 /usr/bin/archphile-optimize
 
@@ -20,36 +20,15 @@ sleep 1
 echo '#New Limits' > /etc/security/limits.conf
 echo '@audio - rtprio 99' >> /etc/security/limits.conf
 echo '@audio - memlock 512000' >> /etc/security/limits.conf
-echo '@audio - nice -20' >> /etc/security/limits.conf
-
-border 'Improving Power Settings'
-sleep 1
-mv /boot/config.txt /boot/config.txt.bak
-wget https://github.com/dynobot/Arch-Linux-Audio-RPi/raw/beta/config.txt -O /boot/config.txt
 
 border 'Improving Network Latency'
 sleep 1
-echo "#New Network Latency" > /etc/sysctl.d/network-latency.conf
-echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
-echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/network-latency.conf
-
-border 'Creating System Service'
-sleep 1
-wget https://github.com/dynobot/Arch-Linux-Audio-RPi/raw/beta/sound.service -O /etc/systemd/system/sound.service
-systemctl enable sound.service
-
-border 'Installing Spotify Client'
-sleep 1
-wget https://github.com/Spotifyd/spotifyd/releases/download/v0.2.19/spotifyd-2019-10-06-armv6-slim.zip
-7z x spoti*
-mv spotifyd /usr/bin/spotifyd
-cd /usr/bin && ./spotifyd
-wget https://github.com/Spotifyd/spotifyd/raw/master/contrib/spotifyd.service -O /etc/systemd/system/spotifyd.service
-mkdir ~/.config/
-cd ~/.config && mkdir spotifyd && cd
-wget https://github.com/dynobot/Arch-Linux-Audio-RPi/raw/beta/spotifyd.conf -O ~/.config/spotifyd/spotifyd.conf
-systemctl start spotifyd.service
-systemctl enable spotifyd.service
+[[ -f /etc/sysctl.d/archphile-network.conf ]] && /etc/sysctl.d/archphile-network.conf /etc/sysctl.d/archphile-network.conf.bak
+echo "#New Network Latency" > /etc/sysctl.d/archphile-network.conf
+echo 'net.core.rmem_max = 16777216' >> /etc/sysctl.d/archphile-network.conf
+echo 'net.core.wmem_max = 16777216' >> /etc/sysctl.d/archphile-network.conf
+echo 'net.ipv4.tcp_rmem= 4096 87380 16777216' >> /etc/sysctl.d/archphile-network.conf
+echo 'net.ipv4.tcp_wmem= 4096 87380 16777216' >> /etc/sysctl.d/archphile-network.conf
 
 border 'Installation Finished'
 border 'Please update spotifyd.conf with your credentials and reboot'
@@ -59,5 +38,5 @@ exit 0
 border 'Cleaning Up & Rebooting System Enjoy the Music'
 sleep 3
 rm basic-install.sh
-border 'Please update spotifyd.conf file located at ~/.config/spotifyd/ then reboot'
+
 #reboot
