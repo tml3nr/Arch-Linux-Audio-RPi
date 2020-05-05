92fd8ac8fc40e63c23409bc5bd7cc0e443a5baa1
Edward <40375833+dynobot@users.noreply.github.com>
2020-05-05 12:42:34 -0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -14,9 +14,11 @@ border 'Removing Linux Audio Tuning'
 [[ -f /etc/sysctl.d/archphile-network.conf ]] && rm /etc/sysctl.d/archphile-network.conf
 [[ -f /etc/security/limits.conf ]] && rm /etc/security/limits.conf
 [[ -f /etc/udev/rules.d/60-mmc-scheduler.rules ]] && rm /etc/udev/rules.d/60-mmc-scheduler.rules
+[[ -f /etc/systemd/system/archphile.service ]] && rm /etc/systemd/system/archphile.service
 [[ -f /etc/security/limits.conf.bak ]] && mv /etc/security/limits.conf.bak /etc/security/limits.conf
 [[ -f /usr/bin/backup-archphile-optimize.bak ]] && mv /usr/bin/backup-archphile-optimize.bak /usr/bin/archphile-optimize
 [[ -f /etc/sysctl.d/archphile-network.conf.bak ]] && mv /etc/sysctl.d/archphile-network.conf.bak /etc/sysctl.d/archphile-network.conf
+[[ -f /etc/systemd/system/backup-archphile.service.bak ]] && mv /etc/systemd/system/backup-archphile.service.bak /etc/systemd/system/archphile.service
 
 border 'Cleaning up & Rebooting System'
 sleep 1
